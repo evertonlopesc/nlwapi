527d3c4f9cea56ea5c019b94357bcf101a92fe1e
Everton Lopes Costa <everton.lopess@outlook.com>
2021-02-23 07:26:30 -0300

    alter - package.json add router get => users.

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,15 @@ import express from 'express';
 
 const app = express();
 
+/**
+ * GET => Buscar
+ * POST => Salvar
+ * PUT => Alterar
+ * DELETE => Deletar
+ * PATCH => Alteração especifica
+ */
+app.get("/users", (request, response) => {
+    return response.json({ message: "Hello world - NLW#04"});
+});
+
 app.listen(3000, () => console.log("Server ir running! (PORT: 3000)"));

b9a208e6d7b54946f22766351bf9958d8010f605
Everton Lopes Costa <everton.lopess@outlook.com>
2021-02-25 06:51:36 -0300

    alter - server.ts include call app.use router and json

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,11 @@
 import 'reflect-metadata';
 import express from 'express';
 import "./database";
+import { router } from './routes';
 
 const app = express();
+
+app.use(express.json());
+app.use(router);
 
 app.listen(3000, () => console.log("Server ir running! (http://localhost:3000)"));

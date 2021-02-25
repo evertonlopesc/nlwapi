ad60b17637e84f98bc49dd1555109f8f03481b53
Everton Lopes Costa <everton.lopess@outlook.com>
2021-02-25 06:33:32 -0300

    error - UserController.ts in import request to Request

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,8 +1,10 @@
-import {request, Response} from "express";
+import {Request, Response} from "express";
 
 class UserController {
     async create(request: Request, response: Response) {
         const body = request.body;
+        console.log(body);
+        return response.send();
     }
 }
 

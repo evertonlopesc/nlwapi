c41fb276b4498259e72e37756a9af7e7e07cf685
Everton Lopes Costa <everton.lopess@outlook.com>
2021-02-25 06:37:56 -0300

    delete - server.ts delete post and get

--- src/server.ts
+++ src/server.ts
@@ -4,23 +4,4 @@ import "./database";
 
 const app = express();
 
-/**
- * GET => Buscar
- * POST => Salvar
- * PUT => Alterar
- * DELETE => Deletar
- * PATCH => Alteração especifica
- */
-app.get("/", (request, response) => {
-    return response.json({ message: "Hello world - NLW#04"});
-});
-
-// 1 param => Rota(Recurso API))
-// 2 param => request, response
-
-app.post("/", (request, response) => {
-    // Recebeu os dados para salvar
-    return response.json({message: "Os dados foram salvos com sucesso!"});
-});
-
 app.listen(3000, () => console.log("Server ir running! (http://localhost:3000)"));

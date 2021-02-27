0e4b3626eb76878a72ceb7a83ea4ea18ce1d8c23
Everton Lopes <everton.lopess@outlook.com>
2021-02-27 09:42:52 -0300

    new file - create a SurveysController.ts

--- src/models/User.ts
+++ src/models/User.ts
@@ -2,24 +2,24 @@ import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
 import { v4 as uuid } from "uuid";
 
 @Entity("users")
-class User{
-    @PrimaryColumn()
-    readonly id: string;
+class User {
+  @PrimaryColumn()
+  readonly id: string;
 
-    @Column()
-    name: string;
+  @Column()
+  name: string;
 
-    @Column()
-    email: string;
+  @Column()
+  email: string;
 
-    @CreateDateColumn()
-    created_at: Date;
+  @CreateDateColumn()
+  created_at: Date;
 
-    constructor() {
-        if(!this.id) {
-            this.id = uuid();
-        }
+  constructor() {
+    if (!this.id) {
+      this.id = uuid();
     }
+  }
 }
 
-export { User }
+export { User };

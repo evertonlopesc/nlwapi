beadc30acc80187ab6b85ba8b5f3edc686d34c5e
Everton Lopes <everton.lopess@outlook.com>
2021-02-28 06:42:09 -0300

    new file: SendMailController.ts

---
+++ src/controllers/SendMailController.ts
@@ -0,0 +1,48 @@
+import { Request, Response } from "express";
+import { getCustomRepository } from "typeorm";
+import { SurveysRepository } from "../repositories/SurveysRepository";
+import { SurveysUsersRepository } from "../repositories/SurveysUsersRepository";
+import { UserRepository } from "../repositories/UserRepository";
+
+class SendMailController {
+    async execute(request: Request, response: Response) {
+        const { email, survey_id } = request.body;
+
+        const usersRepository = getCustomRepository(UserRepository);
+        const surveysRepository = getCustomRepository(SurveysRepository);
+        const surveysUsersRepository = getCustomRepository(
+            SurveysUsersRepository
+        );
+
+        const userAlreadyExists = await usersRepository.findOne({ email });
+
+        if (!userAlreadyExists) {
+            return response.status(400).json({
+                error: "User does not exists",
+            });
+        }
+
+        const surveyAlreadyExists = await surveysRepository.findOne({
+            id: survey_id,
+        });
+
+        if (!surveyAlreadyExists) {
+            return response.status(400).json({
+                error: "Survey does not exists!",
+            });
+        }
+
+        // Salvar
+        const surveyUser = surveysUsersRepository.create({
+            user_id: userAlreadyExists.id,
+            survey_id,
+        });
+        await surveysUsersRepository.save(surveyUser);
+
+        //Enviar
+
+        return response.json(surveyUser);
+    }
+}
+
+export { SendMailController };

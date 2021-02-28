ca495b276036c66de2d1319b3d9f094075c7509c
Everton Lopes <everton.lopess@outlook.com>
2021-02-28 00:40:45 -0300

    modified: add status(201) in return json

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -3,32 +3,31 @@ import { getCustomRepository } from "typeorm";
 import { UserRepository } from "../repositories/UserRepository";
 
 class UserController {
-    async create(request: Request, response: Response) {
-        const { name, email } = request.body;
+  async create(request: Request, response: Response) {
+    const { name, email } = request.body;
 
-        const userRepository = getCustomRepository(UserRepository);
+    const userRepository = getCustomRepository(UserRepository);
 
-        //SELECT * FROM USERS WHERE EMAIL = "EMAIL"
-        const userAlreadyExists = await userRepository.findOne({
-            email,
-        });
+    //SELECT * FROM USERS WHERE EMAIL = "EMAIL"
+    const userAlreadyExists = await userRepository.findOne({
+      email,
+    });
 
-        if (userAlreadyExists) {
-            return response.status(400).json({
-                error: "User already exists!",
-                
-            });
-        }
-
-        const user = userRepository.create({
-            name, 
-            email,
-        });
-
-        await userRepository.save(user);
-
-        return response.json(user);
+    if (userAlreadyExists) {
+      return response.status(400).json({
+        error: "User already exists!",
+      });
     }
+
+    const user = userRepository.create({
+      name,
+      email,
+    });
+
+    await userRepository.save(user);
+
+    return response.status(201).json(user);
+  }
 }
 
 export { UserController };

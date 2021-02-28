ea924b461c3e3a0c91bdec70f9a30ff31c0012af
Everton Lopes <everton.lopess@outlook.com>
2021-02-28 00:37:02 -0300

    modified: import database to createConnection database

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
 import "reflect-metadata";
 import express from "express";
-import "./database";
+import createConnection from "./database";
 import { router } from "./routes";
 
+createConnection();
 const app = express();
 
 app.use(express.json());
